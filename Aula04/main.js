b0d56751d8ba588f9c1ae18b3071424c4ba596ce
Keu Santos <modulodigital69@gmail.com>
2022-03-11 23:57:59 -0300

    estrutura

--- Aula04/main.js
+++ Aula04/main.js
@@ -1,7 +1,14 @@
 function adicionarFilme() {
-  var campoFilmeFavorito = document.getElementById("filme").value;
-  var elementoFilmeFavorito = "<img src=" + campoFilmeFavorito + ">"
-  var listaFilmes = document.getElementById("listaFilmes")
-} 
+  var NomeDoFilme = document.getElementById("filmeNome").value;
+  var filmeFavorito = document.getElementById("filme").value;
+  if (filmeFavorito.endsWith(".jpg")) {
+    listarFilmesNaTela(filmeFavorito);
+  } else {
+    //    console.error("Endereço de filme inválido");
+    alert("Endereço de filme inválido");
+  }
+  document.getElementById("filme").value = "";
+  document.getElementById("filmeNome").value = "";
+}
 
 console.log("campoFilmeFavorito");
