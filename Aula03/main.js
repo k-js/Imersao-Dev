5eaf1d812934a7fbb0901b0269dba6ca246e70af
Keu Santos <modulodigital69@gmail.com>
2022-03-11 22:57:48 -0300

    lista

---
+++ Aula03/main.js
@@ -0,0 +1,8 @@
+
+var listaFilmes = ["Yesterday", "A chegada", "Escola de rocke", "Harry Potter 2"]
+
+// experimente h1 , strong
+document.write("<p>" + listaFilmes[0] + "<p>");
+document.write("<p>" + listaFilmes[1] + "<p>");
+document.write("<p>" + listaFilmes[2] + "<p>");
+document.write("<p>" + listaFilmes[3] + "<p>");
