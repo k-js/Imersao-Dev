4f76a62c4e4212290bbba800cfb4d126ceb76832
Keu Santos <modulodigital69@gmail.com>
2022-03-11 23:26:38 -0300

    adc variavel lista de filmes

--- Aula03/main.js
+++ Aula03/main.js
@@ -1,18 +1,27 @@
 
-var listaFilmes = ["Yesterday", "A chegada", "Escola de rocke", "Harry Potter 2"]
+/**var listaFilmes = ["Yesterday", "A chegada", "Escola de rocke", "Harry Potter 2"]
 // elemento          1               2              3               4
 // índece            0               1              2               3
 
 //adc novos elemntos
 listaFilmes.push("Harru 2");
+listaFilmes.push("Harru 3");
+
+
 
 // experimente h1 , strong
-/**document.write("<p>" + listaFilmes[0] + "<p>");
+document.write("<p>" + listaFilmes[0] + "<p>");
 document.write("<p>" + listaFilmes[1] + "<p>");
 document.write("<p>" + listaFilmes[2] + "<p>");
 document.write("<p>" + listaFilmes[3] + "<p>");
-document.write("<p>" + listaFilmes[4] + "<p>"); */
+document.write("<p>" + listaFilmes[4] + "<p>"); 
 
-for( var indice = 0; indece < 5; indice = indice + 1) {
+for( var indice = 0; indice < listaFilmes.length; indice++) {
   document.write("<p>" + listaFilmes[indice] + "<p>")
 }
+*/
+ var listaFilmes = [
+   "https://capas-p.imagemfilmes.com.br/164777_000_p.jpg", "https://capas-p.imagemfilmes.com.br/164041_000_p.jpg", "https://www.google.com/url?sa=i&url=https%3A%2F%2Fimagemfilmes.com.br%2Ffilmes&psig=AOvVaw2ae8QA7zLxiJizvNX4PNxT&ust=1647138006454000&source=images&cd=vfe&ved=0CAsQjRxqFwoTCIjx1Z_Cv_YCFQAAAAAdAAAAABBG",
+   "https://capas-p.imagemfilmes.com.br/164834_000_p.jpg"
+                    ]
+
