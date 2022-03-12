d86fa1245eab6f21cd2855d3ae490f25ce490e47
Keu Santos <modulodigital69@gmail.com>
2022-03-11 23:08:35 -0300

    laco de repeticao

--- Aula03/main.js
+++ Aula03/main.js
@@ -1,8 +1,18 @@
 
 var listaFilmes = ["Yesterday", "A chegada", "Escola de rocke", "Harry Potter 2"]
+// elemento          1               2              3               4
+// índece            0               1              2               3
+
+//adc novos elemntos
+listaFilmes.push("Harru 2");
 
 // experimente h1 , strong
-document.write("<p>" + listaFilmes[0] + "<p>");
+/**document.write("<p>" + listaFilmes[0] + "<p>");
 document.write("<p>" + listaFilmes[1] + "<p>");
 document.write("<p>" + listaFilmes[2] + "<p>");
 document.write("<p>" + listaFilmes[3] + "<p>");
+document.write("<p>" + listaFilmes[4] + "<p>"); */
+
+for( var indice = 0; indece < 5; indice = indice + 1) {
+  document.write("<p>" + listaFilmes[indice] + "<p>")
+}
