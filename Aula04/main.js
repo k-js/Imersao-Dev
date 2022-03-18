4f71dddba773945352f619018b523514733f269c
Keu Santos <modulodigital69@gmail.com>
2022-03-18 11:22:38 -0300

    Aula04

--- Aula04/main.js
+++ Aula04/main.js
@@ -1,14 +1,16 @@
 function adicionarFilme() {
-  var NomeDoFilme = document.getElementById("filmeNome").value;
   var filmeFavorito = document.getElementById("filme").value;
-  if (filmeFavorito.endsWith(".jpg")) {
-    listarFilmesNaTela(filmeFavorito);
+  if(filmeFavorito.endsWith(".jpg")) {
+    listaFilmesNaTela(filmeFavorito);
   } else {
-    //    console.error("Endereço de filme inválido");
-    alert("Endereço de filme inválido");
-  }
+    console.error("Endereço de filme inválido");
+  } 
   document.getElementById("filme").value = "";
-  document.getElementById("filmeNome").value = "";
 }
 
-console.log("campoFilmeFavorito");
+function listaFilmesNaTela(filme) {
+  var elementoFilmeFavorito = "<img src=" + filme + ">";
+  var elementoListaFilmes = document.getElementById("listaFilmes");
+  elementoListaFilmes.innerHTML = elementoListaFilmes.innerHTML + elementoFilmeFavorito;
+}
+
